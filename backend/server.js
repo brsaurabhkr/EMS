23094c669fbf2fdabe0721aafdf8094ef29904e5
Br-Saurabh-Kumar <saurabhkumarsingh7457@gmail.com>
2026-07-28 12:05:24 +0530

    / route

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const app = require("./app")
 
 const PORT = 5000
 
+app.get('/',(req,res)=>{
+    res.send({
+        activeStatus:true,
+        error:false,
+    })
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server is Running on ${PORT}`)
