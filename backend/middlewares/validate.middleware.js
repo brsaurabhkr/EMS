ff19a9652b6dd0d18f7c2a38c29002992de29b3d
Br-Saurabh-Kumar <saurabhkumarsingh7457@gmail.com>
2026-07-28 11:15:36 +0530

    Update Middleware validation

--- backend/middlewares/validate.middleware.js
+++ backend/middlewares/validate.middleware.js
@@ -6,7 +6,7 @@ const validate = (schema) => {
             return res.status(400).json({
                 success: false,
                 message: "Validation failed",
-                errors: result.error.errors.map((error) => ({
+                errors: result.error.issues.map((error) => ({
                     field: error.path.join("."),
                     message: error.message,
                 })),
